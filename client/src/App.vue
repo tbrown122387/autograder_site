6a3028f673c4d77b4e9d6cf735c0f22870f22acc
Nithin Vijayakumar <nithin2463@gmail.com>
2021-09-01 22:15:43 -0400

    frontend setup

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-app-bar app color="white" flat>
+      <v-tabs centered class="ml-n9" color="grey darken-1">
+        <v-tab to="/">Home</v-tab>
+        <v-tab to="/about">About</v-tab>
+      </v-tabs>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
---
+++ client/src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-card elevation="2">
+      <v-card-title>Card Title</v-card-title>
+      <v-card-text>{{ text }}</v-card-text>
+      <v-card-actions>
+        <v-btn text color="teal" @click="sendRequest">Submit Request</v-btn>
+        <v-btn text color="teal" @click="resetText">Reset Request</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: () => {
+    return {
+      text: "Send a request to change this text",
+    };
+  },
+  methods: {
+    sendRequest() {
+      process.env.VUE_APP_API_ENDPOINT;
+      axios
+        .get(process.env.VUE_APP_API_ENDPOINT + "/api/sample")
+        .then((response) => (this.text = response.data));
+    },
+    resetText() {
+      this.text = "Send a request to change this text";
+    },
+  },
+};
+</script>
+
