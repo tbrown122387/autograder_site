4e9c01fdf41bc6921d599de206fd0d2b3516008e
nithvijay <nv2ba@virginia.edu>
2021-12-25 00:58:17 -0500

    added alembic - database migrations

--- server/app/db.py
+++ server/app/db.py
@@ -4,21 +4,29 @@ import os
 from sqlmodel import create_engine, SQLModel
 
 
-DATABASE_URL: Optional[str] = os.environ.get("DATABASE_URL")
+def get_database_uri() -> str:
+    DATABASE_URL: Optional[str] = os.environ.get("DATABASE_URL")
 
-if not DATABASE_URL:
-    POSTGRES_USER = os.environ.get('POSTGRES_USER')
-    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-    POSTGRES_DB = os.environ.get('POSTGRES_DB')
-    POSTGRES_ADDRESS = os.environ.get('POSTGRES_ADDRESS')
+    if not DATABASE_URL:
+        # local
+        POSTGRES_USER = os.environ.get('POSTGRES_USER')
+        POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+        POSTGRES_DB = os.environ.get('POSTGRES_DB')
+        POSTGRES_ADDRESS = os.environ.get('POSTGRES_ADDRESS')
 
-    # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-    DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_ADDRESS}:5432/{POSTGRES_DB}"
-elif DATABASE_URL.startswith("postgres://"):
-    # Change in SQLAlchemy 1.4
-    # https://help.heroku.com/ZKNTJQSK/why-is-sqlalchemy-1-4-x-not-connecting-to-heroku-postgres
-    DATABASE_URL = DATABASE_URL.replace(
-        "postgres://", "postgresql+psycopg2://")
+        # Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+        DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_ADDRESS}:5432/{POSTGRES_DB}"
+    elif DATABASE_URL.startswith("postgres://"):
+        # prod
+        # Change in SQLAlchemy 1.4
+        # https://help.heroku.com/ZKNTJQSK/why-is-sqlalchemy-1-4-x-not-connecting-to-heroku-postgres
+        DATABASE_URL = DATABASE_URL.replace(
+            "postgres://", "postgresql+psycopg2://")
+    return DATABASE_URL
+
+
+DATABASE_URL = get_database_uri()
+
 
 engine = create_engine(DATABASE_URL, echo=False)
 
