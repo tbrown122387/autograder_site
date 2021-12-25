7857d865fcb44330bd6b2ab200f6e38ef0bce2ae
nithvijay <nv2ba@virginia.edu>
2021-12-25 00:59:16 -0500

    moved database init to alembic

--- server/app/main.py
+++ server/app/main.py
@@ -32,9 +32,9 @@ def health():
     return {"health": "healthy"}
 
 
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
+# @app.on_event("startup")
+# def on_startup():
+#     create_db_and_tables()
 
 
 class DeleteID(BaseModel):
