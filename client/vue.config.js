850e29056e96530bac2fda2249ac60a68b42fb29
Nithin Vijayakumar <nithin2463@gmail.com>
2022-05-02 13:27:49 -0400

    gh-pages deploy

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
+  // transpileDependencies: ["vuetify"],
   pages: {
     index: {
       entry: "src/main.js",
       title: "Autograding Bundler",
     },
   },
-  publicPath: "/",
-  // publicPath: process.env.NODE_ENV === "production" ? "/autograder_site/" : "/",
+  // publicPath: "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/autograder_site/" : "/",
 };
