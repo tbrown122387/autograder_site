3c6aabb244d21afc903e81be2c76717cebd41bbd
Nithin Vijayakumar <nithin2463@gmail.com>
2022-05-03 21:25:44 -0400

    gh-pages deploy

--- client/src/env.js
+++ client/src/env.js
@@ -1,4 +1,4 @@
-const envApiUrl = process.env.NODE_ENV === "production" ? "https://autograding-site.herokuapp.com" : "http://localhost:5001";
+const envApiUrl = process.env.NODE_ENV === "production" ? "https://autograder-site.herokuapp.com" : "http://localhost:5001";
 const API_V1 = "/api/v1";
 
 export const apiUrl = envApiUrl;
